17ee9f030296ff84d999d7fe8b7f49fb8dff4b1d
Allen <glux7381@gmail.com>
2020-08-02 20:53:20 +0800

    Updates

---
+++ precache-manifest.9c2643e3b029efc9d6d83788ae8fb873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30687714f5c17df61b8e9bcd4830b455",
+    "url": "/url-shortener/index.html"
+  },
+  {
+    "revision": "ec0bc0e01d31df3a7200",
+    "url": "/url-shortener/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "6bdf4b991c826048441e",
+    "url": "/url-shortener/static/css/main.eae42496.chunk.css"
+  },
+  {
+    "revision": "ec0bc0e01d31df3a7200",
+    "url": "/url-shortener/static/js/2.952efa18.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/url-shortener/static/js/2.952efa18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdf4b991c826048441e",
+    "url": "/url-shortener/static/js/main.1e5592b3.chunk.js"
+  },
+  {
+    "revision": "d290fa30711f625a3aea",
+    "url": "/url-shortener/static/js/runtime-main.7d8c650d.js"
+  }
+]);
